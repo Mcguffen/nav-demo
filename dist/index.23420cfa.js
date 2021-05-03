26d44e72a32a9138910206df4a813593066785c1
Mcguffen <504177380@qq.com>
2021-05-03 15:05:36 +0800

    添加onerror监听事件，解决用户编辑网址不更新，需要刷新页面的bug

--- dist/index.23420cfa.js
+++ dist/index.23420cfa.js
@@ -486,14 +486,14 @@ var hash = {
         '`':'网址','1':'网址','2':'网址','3':'网址','4':'网址','5':'网址','6':'网址',
         '7':'网址','8':'网址','9':'网址','0':'网址','-':'网址','=':'网址','Back':'网址','Home':'网址',
     
-        'Tab':'网址','q':'qq.com','w':'weibo.com','e':'网址','r':'网址','t':'网址','y':'网址',
-        'u':'网址','i':'网址','o':'网址','p':'网址','[':'网址',']':'网址','\\':'网址','Up':'网址',
+        'Tab':'网址','q':'qq.com','w':'weibo.com','e':'','r':'网址','t':'','y':'youtube.com',
+        'u':'网址','i':'iconfont.cn','o':'网址','p':'网址','[':'网址',']':'网址','\\':'网址','Up':'网址',
     
-        'Ctrl':'网址','a':'网址','s':'网址','d':'网址','f':'网址','g':'网址','h':'网址',
-        'j':'网址','k':'网址','l':'网址',';':'网址','\'':'网址','Enter':'网址','Down':'网址',
+        'Ctrl':'网址','a':'acfun.com','s':'sm.ms','d':'dribble.com','f':'','g':'github.com','h':'网址',
+        'j':'jdbbs.com','k':'网址','l':'网址',';':'网址','\'':'网址','Enter':'网址','Down':'网址',
     
-        'Shift':'网址','z':'网址','x':'网址','c':'网址','v':'网址','b':'网址','n':'网址',
-        'm':'网址',',':'网址','.':'网址','/':'网址','Shift':'网址','↑':'网址','End':'网址',
+        'Shift':'网址','z':'网址','x':'网址','c':'cssgradient.io','v':'v2ex.com','b':'bilili.com','n':'网址',
+        'm':'mcguffen.github.io',',':'网址','.':'网址','/':'网址','Shift':'网址','↑':'网址','End':'网址',
     
         'Caps':'网址','option':'网址','command':'网址','long':'网址','Alt':'网址','Fn':'网址',
         'Ctrl':'网址','←':'网址','↓':'网址','→':'网址',
@@ -530,17 +530,41 @@ while(index < key.length){
         buttonEdit = document.createElement('button')
         buttonEdit.textContent = "edit"
         buttonEdit.id = row[index2]
-        buttonEdit.onclick = function(fdsfsfsf){
-            // 用户输入的键
-            key = fdsfsfsf.target.id
+        // 添加网址icon图标
+        imgIcon = document.createElement('img')
+        if(hash[row[index2]]){
+            imgIcon.src = 'http://' + hash[row[index2]] + '/favicon.ico'
+        }else{
+            imgIcon.src = '//i.bmp.ovh/imgs/2021/05/4871781e4fa1f4d3.jpg'
+            console.log('上面图片一定成功，如果失败说明图挂了。')
+        }
+        imgIcon.onerror = function(e){
+            console.log("下载失败了,帮你添加一个肯定成功的favicon.ico,地址为https://i.bmp.ovh/imgs/2021/05/4871781e4fa1f4d3.jpg")
+            e.target.src = '//i.bmp.ovh/imgs/2021/05/4871781e4fa1f4d3.jpg'
+        }
+
+
+        buttonEdit.onclick = function(e){
+            // 用户输入的键 e[target]就是用户点击的元素
+            button =  e.target
+            key = button.id
+            // 就是在用户点击button的时候重新获取imgIconNew图片地址
+            imgIconNew = button.previousSibling
             userWebsiteEdit = prompt('请输入您想输入的网址')
             // 将用户输入的网址存入对应的hash中去
             hash[key] = userWebsiteEdit // hash 变更
+            imgIconNew.src = 'http://' + userWebsiteEdit + '/favicon.ico'
+            imgIconNew.onerror = function(e){
+                console.log("下载失败了,帮你添加一个肯定成功的favicon.ico,地址为https://i.bmp.ovh/imgs/2021/05/4871781e4fa1f4d3.jpg")
+                e.target.src = '//i.bmp.ovh/imgs/2021/05/4871781e4fa1f4d3.jpg'
+            }
+
             // 只要用户修改website 就将修改后的hash存入'userWebsiteEdit'保存
             // 由于这个方法只接受参数是string所以将hash转换成字符串存入
             localStorage.setItem('userWebsiteEdit',JSON.stringify(hash))
-            console.log(hash);
+            // console.log(hash);
         }
+        kbd.appendChild(imgIcon)
         kbd.appendChild(buttonEdit)
         div.appendChild(kbd)
         index2 += 1
@@ -551,9 +575,9 @@ while(index < key.length){
 
 }
 
-document.onkeypress = function(fdsfsfsf){
+document.onkeypress = function(e){
     // 监听键盘按下的按键
-        key = fdsfsfsf['key'] // qwertyuiop[]\
+        key = e['key'] // qwertyuiop[]\
         website = hash[key]
         console.log(website)
         // 将当前浏览器页面地址换成我们按下按键对应的地址
